ab166640b47ef76760ed616ec89c007371d87460
Deepak <Deepak.Rastogi7@gmail.com>
2018-10-13 13:07:16 +0530

    First Commit

--- ClaimController/src/main/java/org/drasto7/claimcontroller/controllers/ClaimController.java
+++ ClaimController/src/main/java/org/drasto7/claimcontroller/controllers/ClaimController.java
@@ -1,10 +1,13 @@
 package org.drasto7.claimcontroller.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.drasto7.claimcontroller.model.Claim;
 import org.drasto7.claimcontroller.response.ClaimResponse;
 import org.drasto7.claimcontroller.service.IClaimService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +25,22 @@ public class ClaimController {
 	@GetMapping("claimBySubject/{subjectId}")
 	public ResponseEntity<List<ClaimResponse>> getClaimBySubject(@PathVariable("subjectId") String subjectNumber)
 	{
-		claimService.getClaimsBySubject(subjectNumber);
-		return null;
+		List<Claim> claimList = claimService.getClaimsBySubject(subjectNumber);
+		List<ClaimResponse> claimResponseList = new ArrayList<ClaimResponse>();
+		for(Claim claim:claimList){
+			ClaimResponse claimResponse= new ClaimResponse();
+			claimResponse.setAuthNumber(claim.getAuthNumber());
+			claimResponse.setClaimAmount(claim.getClaimAmount());
+			claimResponse.setClaimEndDate(claim.getClaimEndDate());
+			claimResponse.setClaimNumber(claim.getClaimNumber());
+			claimResponse.setClaimStartDate(claim.getClaimStartDate());
+			claimResponse.setClaimStatus(claim.getClaimStatus());
+			claimResponse.setSubjectId(claim.getSubjectId());
+			claimResponseList.add(claimResponse);
+		}
+		ResponseEntity<List<ClaimResponse>> response =
+				new ResponseEntity<List<ClaimResponse>>(claimResponseList,HttpStatus.FOUND);
+		return response;
 	
 		
 	}
